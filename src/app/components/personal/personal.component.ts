f6d4bd8a8ae4f4f957c05853292549c2b881af93
Gonzalo <gonzamaunas@gmail.com>
2023-05-07 03:09:59 -0300

    Agregada Información Personal

---
+++ src/app/components/personal/personal.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Persona } from 'src/app/interfaces';
+
+@Component({
+  selector: 'app-personal',
+  templateUrl: './personal.component.html',
+  styleUrls: ['./personal.component.css']
+})
+export class PersonalComponent {
+
+  @Input()
+  persona: Persona = {} as Persona;
+  edad: number = 23;
+
+}
--- src/app/interfaces.ts
+++ src/app/interfaces.ts
@@ -3,3 +3,15 @@ export interface Contacto{
     nombreContacto: string;
     enlaceContacto: string;
 }
+export interface Persona{
+    idPersona: number;
+    nombreCompleto: string;
+    nacimiento: Date;
+    profesion: string;
+    descripcion: string;
+    urlFoto: string;
+    urlBanner: string;
+}
+export interface Portfolio{
+    persona: Persona;
+}
